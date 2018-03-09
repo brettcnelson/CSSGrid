2a5fd41258f134d25c12b11fcea0b1926853f218
Brett Nelson <brettcnelson@gmail.com>
2018-03-08 22:05:52 -0500

    adds grid

---
+++ main.js
@@ -0,0 +1,16 @@
+var grid = document.getElementById('grid');
+
+for (var i = 0 ; i < 12 ; i++) {
+
+}
+
+
+
+// var grid = document.getElementById('grid');
+// var id = 1;
+// for (var i = 0 ; i < 225 ; i++) {
+// 	var div = document.createElement('DIV');
+// 	div.classList.add('block');
+// 	Math.random() < .1 && div.appendChild(document.createTextNode(id++))
+// 	grid.appendChild(div)
+// }
